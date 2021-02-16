f1a3a3bb22f8a6a08fb76083e8c43e71a94a08ff
akshay / wildfire <akshay.wildfire@pm.me>
2021-02-16 17:56:17 +0530

    backup 7z encrypt based on db pass

--- index.php
+++ index.php
@@ -1,25 +1,30 @@
 <?php
 include_once __DIR__ . '/init.php';
+use Ifsnop\Mysqldump as IMysqldump;
 
 $ignored_files = array();
 $ignored_files[] = 'node_modules/*';
 $ignored_files[] = 'vendor/*';
 $ignored_files[] = '.git/*';
 
-if (($_ENV['S3_BKUP_ACCESS_KEY'] ?? false) && ($_ENV['S3_BKUP_BUCKET_NAME'] ?? false) && ($_ENV['S3_BKUP_HOST_BUCKET'] ?? false) && ($_ENV['S3_BKUP_SECRET_KEY'] ?? false) && ($_ENV['S3_BKUP_HOST_BASE'] ?? false) && defined('ABSOLUTE_PATH')) {
+if (($_ENV['S3_BKUP_ACCESS_KEY'] ?? false) && ($_ENV['S3_BKUP_BUCKET_NAME'] ?? false) && ($_ENV['S3_BKUP_HOST_BUCKET'] ?? false) && ($_ENV['S3_BKUP_SECRET_KEY'] ?? false) && ($_ENV['S3_BKUP_HOST_BASE'] ?? false) && defined('ABSOLUTE_PATH') && ($_ENV['DB_NAME'] ?? false) && ($_ENV['DB_USER'] ?? false) && ($_ENV['DB_PASS'] ?? false)) {
 
-	linux_command('s3cmd sync -r --delete-removed --exclude "' . implode('" --exclude "', $ignored_files) . '" --host="' . $_ENV['S3_BKUP_HOST_BASE'] . '" --access_key="' . $_ENV['S3_BKUP_ACCESS_KEY'] . '" --secret_key="' . $_ENV['S3_BKUP_SECRET_KEY'] . '" --host-bucket="' . $_ENV['S3_BKUP_HOST_BUCKET'] . '" ' . ABSOLUTE_PATH . '/ s3://' . $_ENV['S3_BKUP_BUCKET_NAME']);
+	$upload_paths = $dash->get_uploader_path();
+	$backupfile = $upload_paths['upload_dir'] . '/backup-' . time() . '-' . $_ENV['DB_NAME'] . '.sql';
 
-	if (($_ENV['DB_NAME'] ?? false) && ($_ENV['DB_USER'] ?? false) && ($_ENV['DB_PASS'] ?? false)) {
-		$backupfile = '/tmp/backup-' . $_ENV['DB_NAME'] . '.sql.gz';
-		linux_command('mysqldump --no-tablespaces -u' . $_ENV['DB_USER'] . ' -p' . $_ENV['DB_PASS'] . ' ' . $_ENV['DB_NAME'] . ' | gzip > ' . $backupfile);
-		linux_command('s3cmd --host="' . $_ENV['S3_BKUP_HOST_BASE'] . '" --access_key="' . $_ENV['S3_BKUP_ACCESS_KEY'] . '" --secret_key="' . $_ENV['S3_BKUP_SECRET_KEY'] . '" --host-bucket="' . $_ENV['S3_BKUP_HOST_BUCKET'] . '" put ' . $backupfile . ' s3://' . $_ENV['S3_BKUP_BUCKET_NAME'] . '/');
-		linux_command('rm ' . $backupfile);
+	try {
+		$dump = new IMysqldump\Mysqldump('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+		$dump->start($backupfile);
+	} catch (\Exception $e) {
+		echo 'mysqldump-php error: ' . $e->getMessage();
+	} finally {
+		linux_command('7z a ' . $backupfile . '.7z ' . $backupfile . ' -p' . $_ENV['DB_PASS'] . '; rm ' . $backupfile . ' ; s3cmd sync -r --delete-removed --exclude "' . implode('" --exclude "', $ignored_files) . '" --host="' . $_ENV['S3_BKUP_HOST_BASE'] . '" --access_key="' . $_ENV['S3_BKUP_ACCESS_KEY'] . '" --secret_key="' . $_ENV['S3_BKUP_SECRET_KEY'] . '" --host-bucket="' . $_ENV['S3_BKUP_HOST_BUCKET'] . '" ' . ABSOLUTE_PATH . '/ s3://' . $_ENV['S3_BKUP_BUCKET_NAME'] . ' ;');
+
+		$dash->push_content(array('type' => $type, 'ignored_files' => json_encode($ignored_files)));
+
+		echo '<h3>Backup successfully initiated. You can close this window.</h3>';
 	}
 
-	$dash->push_content(array('type' => $type, 'ignored_files' => json_encode($ignored_files)));
-
-	echo '<h3>Backup successfully initiated. You can close this window.</h3>';
 } else {
 	echo '<h3>S3 credentials have not been added to the dot-env file. Contact your development team.</h3>';
 }
