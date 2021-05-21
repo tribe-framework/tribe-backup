f21c17b6f77d960ba9f78feaf7d678328779d81d
akshay / tw <akshay.wildfire@pm.me>
2021-05-21 09:44:29 +0530

    max script execution time 10 mins

--- init.php
+++ init.php
@@ -1,10 +1,12 @@
 <?php
 namespace Wildfire\Core;
 
+//max script execution time 10 mins
+set_time_limit(600);
+
 $sql = new MySQL();
 $dash = new Dash();
 $admin = new Admin();
-$theme = new Theme();
 
 $type = 'mysql_backup';
 $types = $dash->getTypes();
@@ -12,5 +14,5 @@ $menus = $dash->getMenus();
 $session_user = $dash->getSessionUser();
 
 if ($types['user']['roles'][$session_user['role_slug']]['role'] != 'admin') {
-	die('You do not have permission for this action');
+    die('You do not have permission for this action');
 }
