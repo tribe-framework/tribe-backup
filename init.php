c5c0278dbca3b66bef6c384846c481d2c81b60c8
akshay / tw <akshay.wildfire@pm.me>
2021-05-28 15:43:15 +0530

    currentUser instead of session_user

--- init.php
+++ init.php
@@ -11,8 +11,8 @@ $admin = new Admin();
 $type = 'mysql_backup';
 $types = $dash->getTypes();
 $menus = $dash->getMenus();
-$session_user = $dash->getSessionUser();
+$currentUser = $auth->getCurrentUser();
 
-if ($types['user']['roles'][$session_user['role_slug']]['role'] != 'admin') {
+if ($types['user']['roles'][$currentUser['role_slug']]['role'] != 'admin') {
     die('You do not have permission for this action');
 }
